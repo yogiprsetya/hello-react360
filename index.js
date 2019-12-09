8d84ba1c8ef06e9975aefa87c00cc961821953a6
Yogi Prasetya <yogiprsetya@gmail.com>
2019-12-09 16:59:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class hello360 extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            HELL YEAHHH
+          </Text>
+          <Text style={styles.greeting}>
+            CEMALT AY
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+    textAlign: 'center'
+  },
+});
+
+AppRegistry.registerComponent('hello360', () => hello360);
